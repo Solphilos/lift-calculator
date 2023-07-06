9c0dc1492f37a4a05cc306919c17a5837fb28a01
solphilos <trichosirius@gmail.com>
2023-07-06 19:26:38 -0400

    created functions for basic calculations

---
+++ script.js
@@ -0,0 +1,31 @@
+const calcWeekOne = (max) => {
+    let setOne = .65 * max;
+    let setTwo = .75 * max;
+    let setThree = .85 * max;
+   console.log(setOne, setTwo, setThree)
+ }
+ 
+
+ const calcWeekTwo = (max) => {
+    let setOne = .70 * max;
+    let setTwo = .80 * max;
+    let setThree = .90 * max;
+   console.log(setOne, setTwo, setThree)
+ }
+
+ const calcWeekThree = (max) => {
+    let setOne = .75 * max;
+    let setTwo = .85 * max;
+    let setThree = .95 * max;
+   console.log(setOne, setTwo, setThree)
+ }
+ 
+ const calcWeekOne = (max) => {
+    let setOne = .40 * max;
+    let setTwo = .50 * max;
+    let setThree = .60 * max;
+   console.log(setOne, setTwo, setThree)
+ }
+ 
+ 
+ 
