d1b450b69371d1053e6053018c71d0b3dd1af288
solphilos <trichosirius@gmail.com>
2023-07-06 20:05:47 -0400

    created function for creating results table

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+
+
+ let results = document.querySelector('.results');
+
+
+const makeResultTable = () => {
+    let weekOne = document.createElement('div');
+    let weekTwo = document.createElement('div');
+    let weekThree = document.createElement('div');
+    let weekFour = document.createElement('div');
+    results.appendChild(weekOne);
+    results.appendChild(weekTwo);
+    results.appendChild(weekThree);
+    results.appendChild(weekFour);
+} 
+
+
+
 const calcWeekOne = (max) => {
     let setOne = .65 * max;
     let setTwo = .75 * max;
     let setThree = .85 * max;
-   console.log(setOne, setTwo, setThree)
+    
  }
  
 
@@ -10,7 +28,7 @@ const calcWeekOne = (max) => {
     let setOne = .70 * max;
     let setTwo = .80 * max;
     let setThree = .90 * max;
-   console.log(setOne, setTwo, setThree)
+   console.log(setOne + " x 5", setTwo + " x 5", setThree + " x 5")
  }
 
  const calcWeekThree = (max) => {
@@ -20,12 +38,29 @@ const calcWeekOne = (max) => {
    console.log(setOne, setTwo, setThree)
  }
  
- const calcWeekOne = (max) => {
+ const calcWeekFour = (max) => {
     let setOne = .40 * max;
     let setTwo = .50 * max;
     let setThree = .60 * max;
    console.log(setOne, setTwo, setThree)
  }
  
+
+ const makeRoutine = (tM) => {
+    calcWeekOne(tM);
+    calcWeekTwo(tM);
+    calcWeekThree(tM);
+    calcWeekFour(tM);
+ }
  
+
  
+
+// gets the value of text input field and populates new div with values
+ const submitData = () => {
+    let input = document.getElementById('maxInput').value;
+    makeRoutine(input);
+    
+ }
+
+
